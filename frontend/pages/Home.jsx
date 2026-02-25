333a2ffabf3aa4d082233e54280d65b2cba02ec8
e1iaas <ekberggasper@gmail.com>
2026-02-25 10:06:27 +0000

    Add pagination and skeleton loader to frontend

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,7 +1,8 @@
 // Home.jsx
 import SearchBar from "../components/SearchBar"
 import ProductCard from "../components/ProductCard"
-import { useState } from "react"
+import SkeletonCard from "../components/SkeletonCard"
+import { useState, useEffect } from "react"
 
 
 
@@ -9,17 +10,34 @@ export default function Home() {
   const [loading, setLoading] = useState(false)
   const [query, setQuery] = useState("")
   const [data, setData] = useState([])
+  const [page, setPage] = useState({ page_number: 1})
+  const [submittedQuery, setSumbittedQuery] = useState("")
 
-  const handleSearch = () => {
+  const fetchProducts = (q, pageNum) => {
     if (loading) return
 
     setLoading(true)
-    fetch(`https://api.mobelquery.com/api/?q=${encodeURIComponent(query)}`)
+    fetch(`https://api.mobelquery.com/api/?q=${encodeURIComponent(q)}&page=${pageNum}`)
       .then(res => res.json())
-      .then(json => setData(json.results))
+      .then(json => {
+        console.log(json)
+        setData(json.results ?? [])
+        setPage(json.page_obj ?? [])
+      })
       .catch(err => console.error(err))
       .finally(() => setLoading(false))
   }
+
+  useEffect(() => {
+    fetchProducts(submittedQuery,page.page_number)
+  },[page.page_number])
+
+  const handleSearch = () => {
+    setSumbittedQuery(query)
+    setPage({page_number: 1})
+    fetchProducts(query,1)
+  }
+
 
 return (
   <main className="flex-1 w-full">
@@ -31,7 +49,8 @@ return (
     {/* Grid feed */}
     <div className="max-w-6xl mx-auto px-6 pb-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {data.map(d => (
+        {loading ? Array(12).fill(0).map((_,i) => <SkeletonCard key={i}/>):
+        data.map(d => (
           <ProductCard
             key={d.id}
             title={d.name}
@@ -43,6 +62,20 @@ return (
           />
         ))}
       </div>
+      <div className="flex justify-center mt-6 space-x-32">
+        {page.has_prev ? (
+          <button onClick={() => setPage(prev => ({ ...prev, page_number: prev.page_number + 1 }))}
+          className="bg-orange-700 text-white text-xs font-semibold px-5 py-2 rounded-xl hover:bg-orange-600 transition-colors">
+           ← Previous</button>
+        ) : null}
+
+        
+       {page.has_next ? (
+          <button onClick={() => setPage(prev => ({ ...prev, page_number: prev.page_number + 1 }))}
+          className="bg-orange-700 text-white text-xs font-semibold px-5 py-2 rounded-xl hover:bg-orange-600 transition-colors">
+           Next →</button>
+        ) : null}
+        </div>
     </div>
   </main>
 )
