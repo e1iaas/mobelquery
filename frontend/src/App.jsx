ddb8e4ea9192fe50ecee1c6980178938ab289092
e1iaas <ekberggasper@gmail.com>
2026-02-27 08:02:19 +0000

    Latest version everything.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import Home from "../pages/Home"
-import Privacy from "../pages/Privacy"
-import Terms from "../pages/Terms"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import Home from "../pages/Home.jsx"
+import Privacy from "../pages/Privacy.jsx"
+import Terms from "../pages/Terms.jsx"
+import Header from "../components/Header.jsx"
+import Footer from "../components/Footer.jsx"
 import {Routes, Route} from "react-router-dom"
 
 function App() {
