97d9992936a226e866f7f8ec334ef04d444ea7b3
e1iaas <ekberggasper@gmail.com>
2026-05-14 11:37:48 +0200

    fix: add search/ path to url

--- backend/search/urls.py
+++ backend/search/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import search_view, health_view
 
 urlpatterns = [
-    path("", search_view),
+    path("search/", search_view),
     path("health/", health_view)
 ]
