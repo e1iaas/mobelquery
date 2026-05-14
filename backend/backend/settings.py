488e021ab222f756a551aa362ee5111ddc235075
e1iaas <ekberggasper@gmail.com>
2026-05-14 09:31:05 +0200

    fix: move secret key to environment variable, prep for railway deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x0u_tw2c4e%r=6le&84=^so0qq)ufr8!-)qpdpoz2xms!u%q$6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['api.mobelquery.com','localhost','127.0.0.1']
 
